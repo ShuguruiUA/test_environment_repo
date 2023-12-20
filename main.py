4aa27f64ce44d05c78bb4fe3a33003b0073229d9
Iurii Popov <141816925+ShuguruiUA@users.noreply.github.com>
2023-12-20 06:31:32 +0200

    added fun save, load and  changed logic in show notes

--- main.py
+++ main.py
@@ -27,16 +27,31 @@ list_notes = ['{:^85}'.format(
     '_'*85)]
 
 
+def save():
+    Notebook.save_to_file(notebook, note_file)
+    # print(f'notebook file {note_file} saved successfuly')
+
+
+def load():
+    Notebook.load_from_file(notebook, note_file)
+    # print(f'notebook file {note_file} loaded successfuly')
+
+
 def main():
+    load()
     while True:
         operation = input('Bond says: ').lower()
 
         if operation.startswith(exit_list):
-            Notebook.save_to_file(notebook, note_file)
+            # Notebook.save_to_file(notebook, note_file)
+            # print(Notebook.save_to_file(notebook, note_file))
+            save()
             quit()
 
         elif operation.startswith('create_note'):
-            x = input("Note's name: ")
+            id_counter = 0
+            # x = input("Note's name: ")
+            x = id_counter
             y = input("Note's title: ")
             z = input("Note's info: ")
 
@@ -50,27 +65,34 @@ def main():
                 else:
                     x.add_teg(tag_adder)
             notebook.add_note(x)
+            id_counter += 1
 
         elif operation.startswith('show_notes'):
 
-            for k, v in notebook.items():
+            for value in notebook.values():
+                print(
+                    f'Title: {value.note_title} | Note: {value.note_body} | Tags: {"; ".join(t for t in value.tags)}')
+
+            # for k, v in notebook.items():
                 #     x = v.note_title
                 #     y = v.note_body
                 #     z = '; '.join(p for p in v.tags)
                 #     list_notes.append('|{:^15}|{:<55}|{:^15}'.format(x, y, z))
                 # return [print(el) for el in list_notes]
-                print(
-                    f'Title: {v.note_title}, Note: {v.note_body} Tags: {"; ".join(p for p in v.tags)}')
+                # print(
+                # f'Title: {v.note_title}, Note: {v.note_body} Tags: {"; ".join(p for p in v.tags)}')
             # print(Notebook.show_all(notebook))
             # for x in note_list:
             #     print(x)
         elif operation.startswith('save_notes'):
-            Notebook.save_to_file(notebook, note_file)
+            save()
 
         elif operation.startswith('load_notes'):
             if not os.path.exists('note_file'):
+                print('The file is not exist')
                 pass
-            Notebook.load_from_file(notebook, note_file)
+            load()
+            print(f'Notes file load sucessful')
 
         elif operation.startswith('find_tag'):
             find_tag = input('Enter the tag: ')
@@ -80,4 +102,5 @@ def main():
             pass
 
 
-main()
+if __name__ == "__main__":
+    main()
